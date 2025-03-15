cbcdc9814bdfdccf947b3b06b5a2e3e53d9dfcc2
yassyass2 <yassinabde@outlook.com>
2025-03-15 23:15:52 +0100

    Kepler template

---
+++ Kepler_PoF/my-react-app/src/kepler.jsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import document from "global/document";
+
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import { connect, Provider } from "react-redux";
+
+import keplerGlReducer, { enhanceReduxMiddleware } from "@kepler.gl/reducers";
+import KeplerGl from "@kepler.gl/components";
+
+import AutoSizer from "react-virtualized/dist/commonjs/AutoSizer";
+
+const reducers = combineReducers({
+  keplerGl: keplerGlReducer.initialState({
+    uiState: {
+      readOnly: false,
+      currentModal: null,
+    },
+  }),
+});
+
+const middleWares = enhanceReduxMiddleware([
+  // Add other middlewares here
+]);
+
+const enhancers = applyMiddleware(...middleWares);
+
+const initialState = {};
+const store = createStore(reducers, initialState, compose(enhancers));
+
+const App2 = () => (
+  <div
+    style={{
+      position: "absolute",
+      top: "0px",
+      left: "0px",
+      width: "100%",
+      height: "100%",
+    }}
+  >
+    <AutoSizer>
+      {({ height, width }) => (
+        <KeplerGl
+          mapboxApiAccessToken="xxx" // Replace with your mapbox token
+          id="map"
+          width={width}
+          height={height}
+        />
+      )}
+    </AutoSizer>
+  </div>
+);
+
+const mapStateToProps = (state) => state;
+const dispatchToProps = (dispatch) => ({ dispatch });
+const ConnectedApp = connect(mapStateToProps, dispatchToProps)(App2);
+const Root = () => (
+  <Provider store={store}>
+    <App2 />
+  </Provider>
+);
+
+export default Root;
