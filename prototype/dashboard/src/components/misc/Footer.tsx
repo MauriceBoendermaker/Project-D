0f6063722abfbb7ceda90792dd7ad934a3c78752
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-02 12:39:08 +0200

    Updated footer

--- prototype/dashboard/src/components/misc/Footer.tsx
+++ prototype/dashboard/src/components/misc/Footer.tsx
@@ -3,9 +3,9 @@ export const Footer = () => {
         <div className="container-fluid footer-parent">
             <footer className="container d-flex justify-content-center">
                 <div className="row">
-                    <div className="col-auto text-center">
-                        <p>&copy; {new Date().getFullYear()} - Logistiek Dashboard</p>
-                        <p>Made by&nbsp;
+                    <div className="col-auto footer-bottom-row text-center">
+                        <p>&copy; {new Date().getFullYear()} - E. Lafeber Internationaal Transporten B.V.</p>
+                        <p>Gemaakt door&nbsp;
                             <a className="btn-link" target="_blank" href="https://github.com/yassyass2">
                                 Yassine
                             </a>,&nbsp;
