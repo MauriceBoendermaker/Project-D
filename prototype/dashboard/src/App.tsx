0b57953f3e21e01894c3fff001d168875d4efe1b
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-02 10:54:11 +0200

    Minor path fix

--- prototype/dashboard/src/App.tsx
+++ prototype/dashboard/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Breadcrumbs } from './components/misc/Breadcrumbs';
-import { Nav } from './components/misc/nav/Nav';
+import { Nav } from './components/nav/Nav';
 import { Footer } from './components/misc/Footer';
 
 import './assets/scss/style.scss';
