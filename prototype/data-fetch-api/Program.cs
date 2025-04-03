982c31e2a57f772819ba05333aeedfcbf9b220f5
Maurice Boendermaker <32638413+MauriceBoendermaker@users.noreply.github.com>
2025-04-03 17:39:51 +0200

    Fixed spelling mistake

--- prototype/data-fetch-api/Program.cs
+++ prototype/data-fetch-api/Program.cs
@@ -3,7 +3,7 @@ using Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IFuelService, JsonFuelService>();
-builder.Services.AddScoped<IJsonSHipmentService, JsonShipmentService>();
+builder.Services.AddScoped<IJsonShipmentService, JsonShipmentService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
