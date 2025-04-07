fff76fcc9a34a9fba7935c6f45f0313f3251e99a
Joost <joostderijcke76@gmail.com>
2025-04-07 17:09:13 +0200

    RitBenzine costen in fuelcontroller
    benzine kosten laten berekenen in fuel controller

--- prototype/data-fetch-api/services/JsonFuelService.cs
+++ prototype/data-fetch-api/services/JsonFuelService.cs
@@ -35,11 +35,35 @@ namespace Services
                 var Vehicle = Vehicles.FirstOrDefault(x => x.VoertuigId == VehicleId);
                 if (Vehicle == null || Vehicle.Ritten == null) return total;
 
-                foreach (Rit rit in Vehicle.Ritten){
+                foreach (Rit rit in Vehicle.Ritten)
+                {
                     total += rit.BrandstofVerbruikL;
                 }
-                return total/Vehicle.Ritten.Count;
+                return total / Vehicle.Ritten.Count;
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading JSON: {ex.Message}");
+                return default;
+            }
+        }
+
+        public async Task<int> GetRitCost(string VehicleId, string RitId)
+        {
+            try
+            {
+                int cost = 0;
+                string jsonVehicles = await File.ReadAllTextAsync("data/brandstof_data.json");
+
+                IEnumerable<Vehicle>? Vehicles = JsonSerializer.Deserialize<IEnumerable<Vehicle>>(jsonVehicles);
+                if (Vehicles == null) return cost;
+
+                var Vehicle = Vehicles.FirstOrDefault(x => x.VoertuigId == VehicleId);
+                if (Vehicle == null || Vehicle.Ritten == null) return cost;
+
+                var Rit = Vehicle.Ritten.FirstOrDefault(x => x.RitId == RitId);
+                return Convert.ToInt32(Rit.BrandstofVerbruikL * 1.8690); // Prijs diesel gemmideld 1,8690 incl. BTW (1,5446 excl.) ANWB.nl geraadpleegd 07.04.2025
             }
             catch (Exception ex)
             {
