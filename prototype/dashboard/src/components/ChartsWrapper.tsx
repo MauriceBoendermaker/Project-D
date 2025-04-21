1c8f41491e9966d0d720b269dc2777a968e68958
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-21 13:36:07 +0200

    Formatting

--- prototype/dashboard/src/components/ChartsWrapper.tsx
+++ prototype/dashboard/src/components/ChartsWrapper.tsx
@@ -34,8 +34,7 @@ export const ChartsWrapper = () => {
                     return (
                         <div
                             key={cfg.id}
-                            className={`chart-container ${isZoomed ? "col-12 zoomed" : isHidden ? "d-none" : "col-md-6"
-                                }`}
+                            className={`chart-container ${isZoomed ? "col-12 zoomed" : isHidden ? "d-none" : "col-md-6"}`}
                         >
                             <GenericChart
                                 title={cfg.title}
