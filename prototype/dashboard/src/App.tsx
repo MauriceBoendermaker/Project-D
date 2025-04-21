2e801537f3ff8a73f8b5aef9e2350464b5bfd105
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-21 13:58:03 +0200

    Moved chart files into its own sub folder

--- prototype/dashboard/src/App.tsx
+++ prototype/dashboard/src/App.tsx
@@ -13,8 +13,8 @@ import { Footer } from './components/misc/Footer';
 
 // Import components
 import { ChartsWrapper } from './components/ChartsWrapper';
-import { FuelChart } from "./components/FuelUsageChart";
-import { TripCostChart } from './components/TripCostChart';
+import { FuelChart } from "./components/charts/FuelUsageChart";
+import { TripCostChart } from './components/charts/TripCostChart';
 
 
 const App: React.FC = () => {
