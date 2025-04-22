5ed7867da8cbdca50ee5a634f3c5c7ff23669f9e
Maurice <mauriceboendermaker@gmail.com>
2025-04-22 16:07:11 +0200

    Updated breadcrumb component to handle multi level routing

--- prototype/dashboard/src/components/misc/Breadcrumbs.tsx
+++ prototype/dashboard/src/components/misc/Breadcrumbs.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 export const Breadcrumbs: React.FC = () => {
     const location = useLocation();
 
-    const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+    const capitalize = (s: string) =>
+        s
+            .split('-')
+            .map(part => part.charAt(0).toUpperCase() + part.slice(1))
+            .join(' ');
 
-    const breadcrumb = location.pathname
-        .split('/')
-        .filter(Boolean)
-        .map(capitalize);
+    const paths = location.pathname.split('/').filter(Boolean);
+
+    const breadcrumbs = paths.map((segment, index) => {
+        const path = '/' + paths.slice(0, index + 1).join('/');
+        return {
+            label: capitalize(segment),
+            path,
+        };
+    });
 
     return (
         <section className="row">
             <div className="col breadcrumbs-container">
-                <span><a href="/" className="btn-simple">Home</a> <i className="fa-solid fa-chevron-right"></i> <a href={location.pathname} className="btn-simple">{breadcrumb}</a></span>
+                <span>
+                    <Link to="/" className="btn-simple">Home</Link>
+                    {breadcrumbs.map((crumb, i) => (
+                        <span key={crumb.path}>
+                            {' '}
+                            <i className="fa-solid fa-chevron-right"></i>{' '}
+                            <Link to={crumb.path} className="btn-simple">
+                                {crumb.label}
+                            </Link>
+                        </span>
+                    ))}
+                </span>
             </div>
         </section>
     );
