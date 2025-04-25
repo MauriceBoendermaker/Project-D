cd260f454bccfbeafa3f3b7186590fbf97e790d6
Thijs-1051036 <1051036@hr.nl>
2025-04-25 10:57:59 +0200

    Added: csproj to test dir

---
+++ prototype/data-fetch-api/integrationTests/UnitTest2.cs
@@ -0,0 +1,15 @@
+namespace integrationTests;
+
+public class UnitTest2
+{
+    [Fact]
+    public void Test1()
+    {
+        int a = 5;
+        int b = 10;
+
+        int result = a + b;
+
+        Assert.Equal(15, result);
+    }
+}
