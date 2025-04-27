91fe3d8374dee3ac61516b83a99f83fdb45e831b
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-27 21:03:51 +0200

    Improved Services

--- prototype/data-fetch-api/services/interfaces/IJsonShipmentService.cs
+++ prototype/data-fetch-api/services/interfaces/IJsonShipmentService.cs
@@ -1,11 +1,14 @@
 using Models;
 
-public interface IJsonShipmentService
+namespace Services
 {
-    Task<IEnumerable<Zending>?> GetAllShipments();
-    Task<int> GetMaxCapacity(int ZendingId);
-    Task<int> GetTotalEmptyMiles();
-    Task<double> GetAverageLoadDegree();
-    Task<double> GetLoadDegree(int ZendingId);
-    Task<List<loadDegree>?> GetTotalLoadDegree();
+    public interface IJsonShipmentService
+    {
+        Task<IEnumerable<Shipment>?> GetAllShipments();
+        Task<int> GetMaxCapacity(int shipmentId);
+        Task<int> GetTotalEmptyMiles();
+        Task<double> GetAverageLoadDegree();
+        Task<double> GetLoadDegree(int shipmentId);
+        Task<List<LoadDegree>?> GetTotalLoadDegree();
+    }
 }
