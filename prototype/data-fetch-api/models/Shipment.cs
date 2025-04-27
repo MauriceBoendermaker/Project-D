83cd4b674e1314ea07ee8c106067c4ad5a9df4ef
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-27 21:19:02 +0200

    Refactored parts of Shipment model

--- prototype/data-fetch-api/models/Shipment.cs
+++ prototype/data-fetch-api/models/Shipment.cs
@@ -1,48 +1,34 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
-using System.Xml.Serialization;
 
 namespace Models
 {
     public class Shipment
     {
         [Key]
-        public int Id { get; set; } // Primary key (auto increment)
+        public int Id { get; set; }
 
-        [JsonPropertyName("zending_id")]
-        [XmlElement("zending_id")]
+        [JsonPropertyName("shipment_id")]
         public int ShipmentId { get; set; }
 
-        private string _vehicleId;
+        [JsonPropertyName("destination")]
+        public string Destination { get; set; } = string.Empty;
 
-        [JsonPropertyName("voertuig_id")]
-        [XmlElement("voertuig_id")]
-        public required string VehicleId
-        {
-            get => _vehicleId;
-            set => _vehicleId = value.Trim();
-        }
-
-        private string _destination;
-        [JsonPropertyName("bestemming")]
-        [XmlElement("bestemming")]
-        public required string Destination { get => _destination; set => _destination = value.Trim(); }
-
-        [JsonPropertyName("max_capaciteit")]
-        [XmlElement("max_capaciteit")]
+        [JsonPropertyName("max_capacity_kg")]
         public int MaxCapacityKg { get; set; }
 
-        [JsonPropertyName("huidige_capaciteit")]
-        [XmlElement("huidige_capaciteit")]
+        [JsonPropertyName("current_load_kg")]
         public int CurrentLoadKg { get; set; }
 
-        [JsonPropertyName("onbenutte_kilometers")]
-        [XmlElement("onbenutte_kilometers")]
+        [JsonPropertyName("empty_kilometers")]
         public int EmptyKilometers { get; set; }
 
-        [JsonPropertyName("created_at")]
-        [XmlElement("created_at")]
-        public string? CreatedAt { get; set; }
+        [ForeignKey(nameof(Vehicle))]
+        public int VoertuigId { get; set; }
+
+        public Vehicle Vehicle { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
