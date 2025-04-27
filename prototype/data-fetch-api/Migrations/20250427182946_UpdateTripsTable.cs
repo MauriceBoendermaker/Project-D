5b3674ddea3b0c473ba113255238c5a021c0fdb2
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-27 20:35:54 +0200

    Auto generated migration files

---
+++ prototype/data-fetch-api/Migrations/20250427182946_UpdateTripsTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace data_fetch_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTripsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VoertuigNummer",
+                table: "Voertuigen",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RitNummer",
+                table: "Ritten",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VoertuigNummer",
+                table: "Voertuigen");
+
+            migrationBuilder.DropColumn(
+                name: "RitNummer",
+                table: "Ritten");
+        }
+    }
+}
