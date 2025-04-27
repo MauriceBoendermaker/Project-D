7e5d2746af9e18d2957a5abc76f571d174a30639
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-27 17:36:54 +0200

    Renamed Rit model to Trip to stay inline with conventions

---
+++ prototype/data-fetch-api/models/Trip.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Models
+{
+    public class Trip
+    {
+        [Key]
+        [JsonPropertyName("rit_id")]
+        public int RitId { get; set; } // Primary key (en auto increment)
+
+        [JsonPropertyName("voertuig_id")]
+        [ForeignKey("Vehicle")]
+        public int VehicleVoertuigId { get; set; } // Foreign key naar Voertuig
+
+        public Vehicle Vehicle { get; set; } = null!;
+
+        [JsonPropertyName("datum")]
+        public DateTime? Datum { get; set; }
+
+        [JsonPropertyName("afstand_km")]
+        public int AfstandKm { get; set; }
+
+        [JsonPropertyName("duur_minuten")]
+        public int DuurMinuten { get; set; }
+
+        [JsonPropertyName("brandstof_verbruik_l")]
+        public int BrandstofVerbruikL { get; set; }
+
+        [JsonPropertyName("bestemming_id")]
+        public int BestemmingId { get; set; } // Foreign key naar Locatie
+
+        [JsonPropertyName("klant_id")]
+        public int KlantId { get; set; } // Foreign key naar Klant
+
+        [JsonPropertyName("chauffeur_id")]
+        public int ChauffeurId { get; set; } // Foreign key naar Medewerker
+
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
