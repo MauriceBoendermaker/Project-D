e85ac6329be417bdb2607dc34dedb77d4644c9ed
Thijs-1051036 <1051036@hr.nl>
2025-05-01 10:45:58 +0200

    Potential fix v3.4

--- prototype/data-fetch-api/integrationTests/TripTest.cs
+++ prototype/data-fetch-api/integrationTests/TripTest.cs
@@ -22,7 +22,7 @@ namespace integrationTests
         [Fact]
         public async Task GetTrips_ReturnsOk() {
             // Arrange
-            var url = "api/ritten/overzicht";
+            var url = "/api/ritten/overzicht";
 
             // Act
             var response = await _client.GetAsync(url);
@@ -32,53 +32,53 @@ namespace integrationTests
         }
 
 
-        // [Fact]
-        // public async Task AddTrip_WhenLoggedInWithJwt_ReturnsOk()
-        // {
-        //     // Arrange
-        //     var loginRequest = new
-        //     {
-        //         username = "username",
-        //         password = "password"
-        //     };
+        [Fact]
+        public async Task AddTrip_WhenLoggedInWithJwt_ReturnsOk()
+        {
+            // Arrange
+            var loginRequest = new
+            {
+                username = "username",
+                password = "password"
+            };
 
-        //     var loginJson = JsonConvert.SerializeObject(loginRequest);
-        //     var loginContent = new StringContent(loginJson, Encoding.UTF8, "application/json");
+            var json = JsonSerializer.Serialize(LoginRequest);
+            var loginContent = new StringContent(json, Encoding.UTF8, "application/json");
 
-        //     var loginResponse = await _client.PostAsync("/api/login", loginContent);
-        //     loginResponse.EnsureSuccessStatusCode();
+            var loginResponse = await _client.PostAsync("/api/login", loginContent);
+            loginResponse.EnsureSuccessStatusCode();
 
             
-        //     var loginResponseString = await loginResponse.Content.ReadAsStringAsync();
-        //     dynamic loginResult = JsonConvert.DeserializeObject(loginResponseString);
-        //     string token = loginResult.token;
+            var loginResponseString = await loginResponse.Content.ReadAsStringAsync();
+            dynamic loginResult = JsonConvert.DeserializeObject(loginResponseString);
+            string token = loginResult.token;
 
-        //     _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-        //     var trip = new
-        //     {
-        //         ritId = 999,
-        //         ritNummer = "RIT-1000",
-        //         datum = DateTime.Now,
-        //         afstandKm = 50,
-        //         brandstofVerbruikL = 5,
-        //         duurMinuten = 60,
-        //         voertuigId = "100",
-        //         kenteken = "XX-1234",
-        //         merk = "Red Bull",
-        //         model = "Racing Car",
-        //         brandstofType = "Petrol"
-        //     };
+            var rit = new
+            {
+                ritId = 999,
+                ritNummer = "RIT-1000",
+                datum = DateTime.Now,
+                afstandKm = 50,
+                brandstofVerbruikL = 5,
+                duurMinuten = 60,
+                voertuigId = "100",
+                kenteken = "XX-1234",
+                merk = "Red Bull",
+                model = "Racing Car",
+                brandstofType = "Petrol"
+            };
 
-        //     var tripJson = JsonConvert.SerializeObject(trip);
-        //     var tripContent = new StringContent(tripJson, Encoding.UTF8, "application/json");
+            var tripJson = JsonConvert.SerializeObject(trip);
+            var tripContent = new StringContent(tripJson, Encoding.UTF8, "application/json");
 
-        //     // Arrange
-        //     var response = await _client.PostAsync("/api/ritten", tripContent);
+            // Arrange
+            var response = await _client.PostAsync("/api/ritten", tripContent);
 
-        //     // Assert
-        //     Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        // }
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
 
 
     }
