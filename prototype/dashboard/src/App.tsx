4c1e15f6420e9e3e4c886edb34409ccac751e078
yassyass2 <yassinabde@outlook.com>
2025-05-22 12:30:47 +0200

    Update App.tsx
    fixed typo

--- prototype/dashboard/src/App.tsx
+++ prototype/dashboard/src/App.tsx
@@ -30,7 +30,11 @@ import { AddTrip } from "./components/planning/AddTrip";
 import { AddVehicle } from "./components/admin/AddVehicle";
 import { AddEmployee } from "./components/admin/AddEmployee";
 import { AddCustomer } from "./components/klanten/AddCustomer";
+<<<<<<< Updated upstream
 
+=======
+import { CustomerOverview } from "components/klanten/CustomerOverview";
+>>>>>>> Stashed changes
 import { Employees } from "components/admin/Employees";
 import { CustomerOverview } from "components/klanten/CustomerOverview";
 
