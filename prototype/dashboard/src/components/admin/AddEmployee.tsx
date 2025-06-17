0f02fd24e67ddb6890bc1b64f37ef3f0dbef3600
Maurice <mauriceboendermaker@gmail.com>
2025-06-17 13:00:52 +0200

    Reapply "Added message to check mailbox"
    This reverts commit c6710b990829816cc94511c072d5d1fa8069e2e6.

--- prototype/dashboard/src/components/admin/AddEmployee.tsx
+++ prototype/dashboard/src/components/admin/AddEmployee.tsx
@@ -149,7 +149,7 @@ export const AddEmployee = () => {
               : "Medewerker toegevoegd!"
           }
           body={
-            error.length > 0 ? error : "De medewerker is succesvol toegevoegd."
+            error.length > 0 ? error : "De medewerker is succesvol toegevoegd. Bekijk uw mail inbox voor een wachtwoord."
           }
           isVisible={error.length > 0 || added}
           onFirstBtnClick={() => {
