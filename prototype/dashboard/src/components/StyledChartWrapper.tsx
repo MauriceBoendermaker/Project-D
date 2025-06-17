0874e7d4be299a5c97e3e192f161200b07b6de62
Maurice <mauriceboendermaker@gmail.com>
2025-06-17 11:57:43 +0200

    Added a button to expand the graph / overview

--- prototype/dashboard/src/components/StyledChartWrapper.tsx
+++ prototype/dashboard/src/components/StyledChartWrapper.tsx
@@ -1,3 +1,5 @@
+import React, { ReactElement } from 'react';
+
 interface StyledChartWrapperProps {
   title: React.ReactNode;
   delayIndex?: number;
@@ -9,12 +11,41 @@ export const StyledChartWrapper: React.FC<StyledChartWrapperProps> = ({
   delayIndex = 0,
   children,
 }) => {
+  let chartUrl: string | undefined;
+  let chartPath: string | undefined;
+
+  if (React.isValidElement(title) && title.type === 'a') {
+    const element = title as ReactElement<{ href: string }>;
+    chartUrl = element.props.href;
+
+    try {
+      chartPath = new URL(chartUrl).pathname;
+    } catch {
+      chartPath = undefined;
+    }
+  }
+
+  const isOnChartPage = chartPath === window.location.pathname;
+
   return (
     <div className={`generic-chart p-4 pb-0 chart-delay-${delayIndex}`}>
-      <div className="d-flex justify-content-between align-items-start mb-4">
-        <div>
-          <h5 className="mb-0">{title}</h5>
-        </div>
+      <div className="d-flex justify-content-between align-items-center mb-4">
+        <h5 className="mb-0">{title}</h5>
+        <a
+          href={isOnChartPage ? '/' : chartUrl}
+          type="button"
+          className="btn-link"
+        >
+          {isOnChartPage ? (
+            <>
+              <i className="fa-solid fa-chevron-left"></i>&nbsp;Terug naar overzicht
+            </>
+          ) : (
+            <>
+              Grafiek uitbreiden&nbsp;<i className="fa-solid fa-chevron-right"></i>
+            </>
+          )}
+        </a>
       </div>
       <div className="chart-content">
         {children}
