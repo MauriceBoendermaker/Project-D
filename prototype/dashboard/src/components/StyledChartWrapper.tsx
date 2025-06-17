ff8e2cf195cfbc081b427b552006788b352936cc
Maurice <mauriceboendermaker@gmail.com>
2025-06-17 11:29:39 +0200

    Removed unnecessary subtitle (rudiment as it is never used)

--- prototype/dashboard/src/components/StyledChartWrapper.tsx
+++ prototype/dashboard/src/components/StyledChartWrapper.tsx
@@ -1,13 +1,11 @@
 interface StyledChartWrapperProps {
   title: React.ReactNode;
-  subtitle?: string;
   delayIndex?: number;
   children: React.ReactNode;
 }
 
 export const StyledChartWrapper: React.FC<StyledChartWrapperProps> = ({
   title,
-  subtitle,
   delayIndex = 0,
   children,
 }) => {
@@ -15,7 +13,6 @@ export const StyledChartWrapper: React.FC<StyledChartWrapperProps> = ({
     <div className={`generic-chart p-4 pb-0 chart-delay-${delayIndex}`}>
       <div className="d-flex justify-content-between align-items-start mb-4">
         <div>
-          <p className="text-muted mb-1">{subtitle}</p>
           <h5 className="mb-0">{title}</h5>
         </div>
       </div>
