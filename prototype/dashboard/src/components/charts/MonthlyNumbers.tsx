0b701678561d04bf28a17347e61a0098083b876f
yassyass2 <yassinabde@outlook.com>
2025-06-23 15:22:52 +0200

    opzet maandelijkse weergave component

---
+++ prototype/dashboard/src/components/charts/MonthlyNumbers.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { StyledChartWrapper } from "../StyledChartWrapper";
+import { MONTHLY_NUMBERS_TITLE } from "components/ChartTitles";
+import { Trip, fetchTripData } from "api/fetchTripData";
+
+interface MonthlyNumbersProps {
+    delayIndex?: number;
+    Data: Trip[];
+}
+
+export type TripResponse = {
+    message: string;
+    data: any[];
+};
+
+export const MonthlyNumbers: React.FC<MonthlyNumbersProps> = ({ delayIndex = 0, Data = [], }) => {
+    const [monthlyData, setmonthlyData] = useState<Trip[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const getTripData = async () => {
+            if (Data.length > 0) {
+                setmonthlyData(Data);
+            } else {
+                try {
+                    const data = await fetchTripData();
+                    setmonthlyData(data);
+                } catch (err: any) {
+                    setError(err.message);
+                }
+            }
+        };
+        getTripData();
+    }, [Data]);
+
+    return (
+        <StyledChartWrapper
+            title={
+                <a
+                    href="http://localhost:5000"
+                    style={{
+                        textDecoration: "none",
+                        color: "inherit",
+                    }}
+                >
+                    {MONTHLY_NUMBERS_TITLE}
+                </a>
+            }
+            delayIndex={delayIndex}
+        >
+            {loading && <div>Laden van data...</div>}
+            {error && <div>Fout: {error}</div>}
+            {!loading && !error && Data.length > 0 && (
+                <div>
+                    <p>Totale ritten: {monthlyData.length}</p>
+                </div>
+            )}
+        </StyledChartWrapper>
+    );
+};
