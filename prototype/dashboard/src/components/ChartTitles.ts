68c15301e9c2ec06404e452494f3e93c1cfd5364
Thijs <1051036@hr.nl>
2025-06-26 11:57:09 +0200

    Changed: names of the chart

--- prototype/dashboard/src/components/ChartTitles.ts
+++ prototype/dashboard/src/components/ChartTitles.ts
@@ -2,6 +2,6 @@ export const FUEL_CHART_TITLE = "Brandstof verbruik per voertuig";
 
 export const LOAD_DEGREE_TITLE = "Beladingsgraad per zending";
 
-export const TRIP_COST_TITLE = "Kosten per rit";
+export const TRIP_COST_TITLE = "Benzinekosten per rit";
 
 export const MONTHLY_NUMBERS_TITLE = "Huidige maand statistieken";
